af5e830efb416425ffd233cea8c1e2ea1916bc99
Andrew Lubinger <45952290+amlubinger@users.noreply.github.com>
2020-10-23 15:40:30 -0400

    Some java stuff for lists and navigation etc.

--- app/src/main/java/com/example/lister/ListFragment.java
+++ app/src/main/java/com/example/lister/ListFragment.java
@@ -1,15 +1,32 @@
 package com.example.lister;
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class ListFragment extends Fragment{
+
+    String list;
+    List<String> itemsList;
+    ArrayAdapter<String> items;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,13 +40,17 @@ public class ListFragment extends Fragment{
         Log.d("Lifecycle", "ListFragment - onCreateView");
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
+        assert getArguments() != null;
+        list = ListFragmentArgs.fromBundle(getArguments()).getList();
+
         //Disable enter button since no list name entered yet
         final Button enterButton = view.findViewById(R.id.enterButton);
         enterButton.setEnabled(false);
         enterButton.setVisibility(View.GONE);
 
         //Enable enter button upon opening keyboard for list name
-        TextView inputListName = view.findViewById(R.id.inputListName);
+        final TextView inputListName = view.findViewById(R.id.inputListName);
+        inputListName.setText(list);
         inputListName.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
             public void onFocusChange(View view, boolean b) {
@@ -38,9 +59,67 @@ public class ListFragment extends Fragment{
                     enterButton.setVisibility(View.VISIBLE);
                 } else {
                     enterButton.setVisibility(View.GONE);
+                    hideKeyboard();
                 }
             }
         });
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputListName.clearFocus();
+            }
+        });
+
+        // Populate list of lists
+        itemsList = new ArrayList<>();
+        itemsList.add("Test Item");
+        itemsList.add("Test Item 2");
+        itemsList.add("Test Item 3");
+        ListView listView = view.findViewById(R.id.listOfItems);
+        if(getContext() != null) {
+            items = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, itemsList);
+            listView.setAdapter(items);
+            setListViewListener(listView, itemsList);
+        }
+
+        view.findViewById(R.id.newItemButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                items.add("New Item");
+            }
+        });
+
         return view;
+    }
+
+    private void setListViewListener(final ListView listView, final List<String> items){
+        //Todo: These items will have other views in them and may actually be a custom list view.
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(getContext() != null) {
+                    final Dialog editListOfList = new Dialog(getContext(), android.R.style.Theme_Black_NoTitleBar);
+                    Objects.requireNonNull(editListOfList.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
+                    //Todo: This should edit a list item, not a list of lists.
+                    editListOfList.setContentView(R.layout.edit_list_of_list);
+                    editListOfList.setCancelable(true);
+                    editListOfList.show();
+                }
+                return true;
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Something maybe?
+            }
+        });
+    }
+
+    public void hideKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager) requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        assert inputMethodManager != null;
+        inputMethodManager.hideSoftInputFromWindow(requireView().getWindowToken(), 0);
     }
 }
